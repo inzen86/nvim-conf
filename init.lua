0e8ace9fd1e0df27630f61f0ef9b7a28aed0cfe8
inzen86 <inzen86@mand.pro>
2023-04-21 23:53:45 +0300

    Setting default shell for windows to powershell. Code copied from toggletrem.nvim

--- init.lua
+++ init.lua
@@ -88,3 +88,20 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+-- Check if Windows
+local is_win32 = vim.fn.has("win32")
+if (is_win32 == 1) then
+    local powershell_options = {
+      shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+      shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+      shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+      shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+      shellquote = "",
+      shellxquote = "",
+    }
+
+    for option, value in pairs(powershell_options) do
+      vim.opt[option] = value
+    end
+end
