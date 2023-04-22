a60401b05b370d746f15945bd8baead2e442cfe7
inzen86 <inzen86@mand.pro>
2023-04-22 20:55:58 +0300

    Changing powershell part a little, I dont like the loop there. also changing comment to mactch my use.

--- init.lua
+++ init.lua
@@ -89,19 +89,13 @@ cmp.setup {
   },
 }
 
--- Check if Windows
+-- If windows set powershell as shell
 local is_win32 = vim.fn.has("win32")
 if (is_win32 == 1) then
-    local powershell_options = {
-      shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-      shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-      shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-      shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-      shellquote = "",
-      shellxquote = "",
-    }
-
-    for option, value in pairs(powershell_options) do
-      vim.opt[option] = value
-    end
+	vim.opt.shell = "powershell"
+	vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+	vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+	vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+	vim.opt.shellquote = ""
+	vim.opt.shellxquote = ""
 end
